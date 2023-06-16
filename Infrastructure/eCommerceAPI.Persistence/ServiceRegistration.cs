f2486d2eb5f2482f1f97eaedbdf4ff3a8f453826
Berkant Gul <berkantgul25@gmail.com>
2023-06-16 11:32:15 +0300

    Connection to PostgreSql database is established and first entities are migrated to database.

---
+++ Infrastructure/eCommerceAPI.Persistence/ServiceRegistration.cs
@@ -0,0 +1,20 @@
+﻿using eCommerceAPI.Persistence.Contexts;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eCommerceAPI.Persistence
+{
+    static public class ServiceRegistration
+    {
+        public static void AddPersistenceService(this IServiceCollection services)
+        {
+            services.AddDbContext<ECommerceAPIDbContext>(options => options.UseNpgsql(Configuration.ConnectionString));
+        }
+    }
+}
